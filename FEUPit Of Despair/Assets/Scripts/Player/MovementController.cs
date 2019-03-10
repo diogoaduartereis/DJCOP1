bd43c3cae5d28a2177b98a4ec81e0fd9caa5c1fd
Tiago Magalhães <up201607931@fe.up.pt>
2019-03-10 14:53:01 +0000

    added prefab for player movement

---
+++ FEUPit Of Despair/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed = 0.15f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       float xMov = Input.GetAxis("Horizontal") * speed;
+       float yMov = Input.GetAxis("Vertical") * speed;
+
+       rb.MovePosition(Player.transform.position + new Vector3(xMov,yMov,0));
+    }
+
+    void Update()
+    {
+        Player.transform.rotation = Quaternion.identity;
+    }
+}
