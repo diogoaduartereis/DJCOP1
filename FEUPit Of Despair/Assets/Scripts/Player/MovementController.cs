095c388518e5da103ca7d2c7f8e8e9a8e30ac172
Tiago Magalhães <up201607931@fe.up.pt>
2019-03-11 20:22:37 +0000

    added a dash to the player

--- FEUPit Of Despair/Assets/Scripts/Player/MovementController.cs
+++ FEUPit Of Despair/Assets/Scripts/Player/MovementController.cs
@@ -7,23 +7,73 @@ public class MovementController : MonoBehaviour
     public GameObject Player;
     public float speed = 0.15f;
 
+    public float DashSpeed = 1f;
+    public float DashCooldown = 5f;
+    public float DashTime = 1f;
+
+    private bool dashing = false;
+    private Vector3 dashDirection;
+    private float currDashCD = 0.0f;
+    private float remainingDashTime = 0.0f;
+
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
        float xMov = Input.GetAxis("Horizontal") * speed;
        float yMov = Input.GetAxis("Vertical") * speed;
+       if (!dashing)
+       {
+           rb.MovePosition(Player.transform.position + new Vector3(xMov, yMov, 0));
+       }
 
-       rb.MovePosition(Player.transform.position + new Vector3(xMov,yMov,0));
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+       {
+           if (!dashing && currDashCD <= 0 && remainingDashTime <= 0)
+           {
+                dashDirection = (new Vector3(xMov,yMov,0)).normalized;
+
+                dashing = true;
+                currDashCD = DashCooldown;
+                remainingDashTime = DashTime;
+           }
+       }
     }
 
     void Update()
     {
         Player.transform.rotation = Quaternion.identity;
+
+        Debug.Log("Dashing?:"+dashing);
+        Debug.Log("Dash CD:" + currDashCD);
+        Debug.Log("Remaninig Dash Time:" + remainingDashTime);
+        Debug.Log("Dash Direction" + dashDirection);
+
+        if (dashing)
+        {
+            rb.MovePosition(Player.transform.position + dashDirection * DashSpeed * Time.deltaTime);
+            remainingDashTime -= Time.deltaTime;
+
+            if (remainingDashTime <= 0)
+            {
+                dashing = false;
+                remainingDashTime = 0.0f;
+                dashDirection = Vector2.zero;
+            }
+        }
+
+        if (currDashCD > 0)
+        {
+            currDashCD -= Time.deltaTime;
+        }
+        else
+        {
+            currDashCD = 0;
+        }
     }
 }
