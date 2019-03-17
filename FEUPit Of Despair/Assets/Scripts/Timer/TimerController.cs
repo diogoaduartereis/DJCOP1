e954d84d1c9508f2c708cc8fbb3f8d1759651b27
up201505472 <up201505472@fe.up.pt>
2019-03-17 01:58:34 +0000

    Added pretty timer, have to fix 60 seconds instead of 59

---
+++ FEUPit Of Despair/Assets/Scripts/Timer/TimerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timerText;
+    public float StartingTime = 20f;
+    private float Seconds;
+    private float FinalSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FinalSeconds = 59;
+        Seconds = 59;
+        this.StartingTime *= 60;
+        StartCoundownTimer();
+    }
+
+    void StartCoundownTimer()
+    {
+        if (timerText != null)
+        {
+            timerText.text = StartingTime + ":00";
+            InvokeRepeating("UpdateTimer", 0.0f, 0.01667f);
+        }
+    }
+
+    void Update()
+    {
+        if (timerText != null)
+        {
+            StartingTime -= Time.deltaTime;
+            string minutes = Mathf.Floor(StartingTime / 60).ToString("00");
+            string seconds = (StartingTime % 60).ToString("00");
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+}
