619df5ace148557100160d7dd2b196a482cd8100
Tiago Magalhães <up201607931@fe.up.pt>
2019-03-18 22:44:59 +0000

    working on object controller

--- FEUPit Of Despair/Assets/Scripts/Objective/ObjectiveController.cs
+++ FEUPit Of Despair/Assets/Scripts/Objective/ObjectiveController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ObjectiveController : MonoBehaviour
 {
+
+    public int WaveCount = 5;
+    public int EnemiesPerWave = 10;
+    public int TimeBetweenWaves = 20;
+
+    private bool active = false;
+
+
     // Update is called once per frame
     void Update()
     {
