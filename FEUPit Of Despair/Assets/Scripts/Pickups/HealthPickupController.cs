e2e7a4a0f7c943e881ccab626ceaa90ba01fe6b2
up201505472 <up201505472@fe.up.pt>
2019-03-19 00:35:34 +0000

    healthpickup work

--- FEUPit Of Despair/Assets/Scripts/Pickups/HealthPickupController.cs
+++ FEUPit Of Despair/Assets/Scripts/Pickups/HealthPickupController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class HealthPickupController : MonoBehaviour
 {
-
+    public AudioSource pickupSound;
     public float HealthRegenValue = 5f;
+
+    private void Start()
+    {
+        pickupSound = this.GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            pickupSound.transform.parent = null;
+            pickupSound.Play();
+            Destroy(pickupSound.gameObject, pickupSound.clip.length);
             GameObject player = other.gameObject;
             HealthController healthController = player.GetComponent<HealthController>();
             healthController.Heal(this.HealthRegenValue);
